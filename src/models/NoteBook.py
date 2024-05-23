239a38435f00179044bf75bf907d5206cc240090
Roman Novak <rnovak99@gmail.com>
2024-05-23 12:11:15 +0300

    Fixed find-note

--- src/models/NoteBook.py
+++ src/models/NoteBook.py
@@ -12,18 +12,20 @@ class NoteBook(Book, UserDict[str, Note]):
         return self.data.get(name)
 
     def find_by_name(self, name:str) -> list[Note]:
-        keys = filter(lambda key: name in key, self.data.keys())
+        keys = filter(lambda key: name.lower() in key.lower(),
+                      self.data.keys())
         return [self.data.get(key) for key in keys]
 
     def delete(self, name:str):
         del self.data[name]
 
     def find_by_text(self, partial_text:str) -> list[Note]:
-        return list(filter(lambda note: partial_text in note.text, self.data.values()))
+        return list(filter(lambda note: partial_text.lower() in note.text.lower(),
+                           self.data.values()))
 
     def get_by_name(self, name:str) -> Note:
         return self.data[name]
-    
+
     @classmethod
     def from_dict(cls, data):
         note_book = cls()
@@ -33,7 +35,6 @@ class NoteBook(Book, UserDict[str, Note]):
         for name, note in book.items():
             note_book[name] = Note.from_dict(name, note)
         return note_book
-    
+
     def to_dict(self) -> dict:
         return { "note-book": {key: value.to_dict() for key, value in self.data.items()}} if self.data else None
-    
